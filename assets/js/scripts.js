cd6bfc16bb1d1d92ae00f4965f9aa172a658feaf
PedroSebastiaan <pizarropedrov@gmail.com>
2021-05-02 11:24:23 -0400

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,12 @@
+// funcion para ejecutar posterior al cargado de la pagina completa
+
+$(function () {
+
+    // Metodos de jQuery...
+
+
+    var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))
+    var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
+        return new bootstrap.Popover(popoverTriggerEl)
+    })
+});
